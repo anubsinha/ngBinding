82896598c4b43b4a58c66b7f4cec24aa9d000eab
Anub Sinha <anub.sinha@opscale.io>
2018-06-15 19:20:17 +0530

    use fontaweome for angular with treeshaking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
+
+
+library.add(faCoffee, faTwitter, faCalendar);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoriteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+  faStar = faStar;
+  isActive = true;
+  toggle(){
+    this.isActive = !this.isActive; 
+    
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
